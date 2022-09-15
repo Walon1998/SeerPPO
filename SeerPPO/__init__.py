cef68ccf4dad46bec0ccee92512b663a81a72681
Neville Walo <neville.walo@gmail.com>
2022-09-15 16:35:35 +0200

    added misc feature encoding

--- SeerPPO/__init__.py
+++ SeerPPO/__init__.py
@@ -2,3 +2,4 @@ from .RolloutBuffer import RolloutBuffer
 from .RolloutBuffer import RolloutBufferSamples
 from .policy import SeerNetwork
 from .masking import create_mask
+from .misc import impute_features, get_encoded_action
