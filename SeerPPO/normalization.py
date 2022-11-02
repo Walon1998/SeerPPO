6fd87d8c1ab8fadd0ad08f55d9c8798d9f78d5bb
Neville Walo <neville.walo@gmail.com>
2022-11-02 22:16:07 +0100

    added v2 first version

--- SeerPPO/normalization.py
+++ SeerPPO/normalization.py
@@ -5,7 +5,27 @@ import torch
 from torch import nn
 
 
-class SeerScaler(nn.Module):
+class Scaler(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.scaler = None
+
+    def forward(self, x):
+        with torch.no_grad():
+
+            if x.is_cuda:
+                device_x = "cuda"
+            else:
+                device_x = "cpu"
+
+            self.scaler = self.scaler.to(device_x)
+
+            x = x * self.scaler
+        return x
+
+
+class SeerScaler(Scaler):
     def __init__(self):
         super().__init__()
 
@@ -132,15 +152,101 @@ class SeerScaler(nn.Module):
 
         assert torch.all(self.scaler <= 1.0)
 
-    def forward(self, x):
-        with torch.no_grad():
 
-            if x.is_cuda:
-                device_x = "cuda"
-            else:
-                device_x = "cpu"
+class BallScalerv2(Scaler):
 
-            self.scaler = self.scaler.to(device_x)
+    def __init__(self):
+        super(BallScalerv2, self).__init__()
 
-            x = x * self.scaler
-        return x
+        self.scaler = torch.tensor([
+            1.0 / 4096.0,  # Pos_x
+            1.0 / 5120.0,  # Pos_y
+            1.0 / 2048.0,  # Pos_z
+            1.0 / 6000.0,  # vel_x
+            1.0 / 6000.0,  # vel_y
+            1.0 / 6000.0,  # vel_z
+            1.0 / 6.0,  # ang_vel_x
+            1.0 / 6.0,  # ang_vel_y
+            1.0 / 6.0,  # ang_vel_z
+            1.0 / 6000.0,  # vel_norm
+        ], dtype=torch.float32, requires_grad=False)
+
+
+class BoostpadsScalerv2(Scaler):
+
+    def __init__(self):
+        super(BoostpadsScalerv2, self).__init__()
+
+        boost_active = [
+            1.0 for _ in range(34)
+        ]
+
+        # timers
+        self.scaler = torch.tensor([
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 10.0,
+                                       1.0 / 10.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 10.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 10.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 10.0,
+                                       1.0 / 10.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                       1.0 / 4.0,
+                                   ] + boost_active, dtype=torch.float32, requires_grad=False)
+
+
+class PlayerEncoder(Scaler):
+
+    def __init__(self):
+        super(PlayerEncoder, self).__init__()
+        self.scaler = torch.tensor([
+            1.0 / 4096.0,  # pos_x
+            1.0 / 5120.0,  # pos_y
+            1.0 / 2048.0,  # pos_z
+            1.0 / math.pi,  # rot_x
+            1.0 / math.pi,  # rot_y
+            1.0 / math.pi,  # rot_z
+            1.0 / 2300.0,  # vel_x
+            1.0 / 2300.0,  # vel_y
+            1.0 / 2300.0,  # vel_z
+            1.0 / 5.5,  # ang_vel_x
+            1.0 / 5.5,  # ang_vel_y
+            1.0 / 5.5,  # ang_vel_z
+            1.0 / 3.0,  # demo timer
+            1.0 / 100.0,  # boost
+            1.0,  # wheel contact
+            1.0,  # has flip
+            1.0,  # alive
+            1.0 / 6000.0,  # vel_norm
+            1.0,  # supersonic
+            1.0 / (4096.0 * 2.0),  # distance_ball_x
+            1.0 / (5120.0 * 2.0),  # distance_ball_y
+            1.0 / 2048.0,  # distance_ball_z
+            1.0 / 13272.55,  # distance_ball_norm
+
+        ], dtype=torch.float32, requires_grad=False)
