7a51223f84bdef6b6eb4b8eee4a349c1fe369ade
Neville Walo <neville.walo@gmail.com>
2022-12-16 20:11:58 +0100

    fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="SeerPPO",
-    version="0.0.29",
+    version="0.0.30",
     url="https://github.com/Walon1998/SeerPPO",
     author="Neville Walo",
     author_email="neville.walo@gmail.com",
